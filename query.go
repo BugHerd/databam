e986e59873153d5b35c932b93d3a6755ff9b6c6b
deoxxa <deoxxa@fknsrs.biz>
2014-11-16 12:33:21 +1100

    move all query generation to query class

--- query.go
+++ query.go
@@ -2,6 +2,7 @@ package databam
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/kr/pretty"
@@ -17,17 +18,48 @@ type queryJoin struct {
 	remote queryField
 }
 
-type queryCondition interface{}
+type queryCondition interface {
+	Compile(*selectQuery) (string, []interface{})
+}
+
+type queryConditionAnd []queryCondition
+
+func (c *queryConditionAnd) Compile(q *selectQuery) (string, []interface{}) {
+	bits := []string{}
+	vals := []interface{}{}
+
+	for _, c := range *c {
+		s, v := c.Compile(q)
+
+		bits = append(bits, s)
+		vals = append(vals, v...)
+	}
+
+	if len(bits) > 1 {
+		return "(" + strings.Join(bits, " AND ") + ")", vals
+	} else {
+		return bits[0], vals
+	}
+}
+
+type queryConditionEq struct {
+	field queryField
+	value interface{}
+}
+
+func (c *queryConditionEq) Compile(q *selectQuery) (string, []interface{}) {
+	return fmt.Sprintf("%s = ?", q.formatField(c.field)), []interface{}{c.value}
+}
 
 type selectQuery struct {
-	model      *Repository
-	counter    int
-	aliases    map[*Repository]string
-	fields     []queryField
-	joins      []queryJoin
-	conditions []queryCondition
-	offset     int
-	limit      int
+	model     *Repository
+	counter   int
+	aliases   map[*Repository]string
+	fields    []queryField
+	joins     []queryJoin
+	condition queryConditionAnd
+	offset    int
+	limit     int
 }
 
 func newSelectQuery(model *Repository) selectQuery {
@@ -65,7 +97,7 @@ func (q selectQuery) Join(j queryJoin) selectQuery {
 }
 
 func (q selectQuery) Where(c queryCondition) selectQuery {
-	q.conditions = append(q.conditions, c)
+	q.condition = append(q.condition, c)
 
 	return q
 }
@@ -84,6 +116,7 @@ func (q selectQuery) Limit(limit int) selectQuery {
 
 func (q selectQuery) Compile() (string, []interface{}, error) {
 	bits := []string{"SELECT"}
+	vals := []interface{}{}
 
 	fields := []string{}
 	for _, f := range q.fields {
@@ -104,10 +137,14 @@ func (q selectQuery) Compile() (string, []interface{}, error) {
 			"JOIN %s %s ON %s = %s",
 			j.remote.model.table,
 			q.aliasFor(j.remote.model),
-			fmt.Sprintf("%s.%s", q.aliasFor(j.remote.model), q.model.d.mapper.FieldToColumn(j.remote.field)),
-			fmt.Sprintf("%s.%s", q.aliasFor(j.source.model), q.model.d.mapper.FieldToColumn(j.source.field)),
+			q.formatField(j.remote),
+			q.formatField(j.source),
 		))
 	}
+
+	b, v := q.condition.Compile(&q)
+	bits = append(bits, "WHERE", b)
+	vals = append(vals, v...)
 
 	if q.offset != 0 {
 		bits = append(bits, fmt.Sprintf("OFFSET %d", q.offset))
@@ -118,5 +155,143 @@ func (q selectQuery) Compile() (string, []interface{}, error) {
 
 	pretty.Println(q)
 
-	return strings.Join(bits, " "), []interface{}{}, nil
+	return strings.Join(bits, " "), vals, nil
+}
+
+func (q selectQuery) formatField(f queryField) string {
+	return fmt.Sprintf(
+		"%s.%s",
+		q.aliasFor(f.model),
+		q.model.d.mapper.FieldToColumn(f.field),
+	)
+}
+
+func (q *selectQuery) addModelToQuery(source *Repository, model interface{}) error {
+	v := reflect.ValueOf(model)
+
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	w := queryConditionAnd{}
+	for i, j := 0, v.NumField(); i < j; i++ {
+		f := v.Field(i)
+
+		if !reflect.DeepEqual(f.Interface(), reflect.Zero(f.Type()).Interface()) {
+			if f.Kind() == reflect.Ptr {
+				f = reflect.Indirect(f)
+			}
+
+			if f.Kind() == reflect.Int || f.Kind() == reflect.String {
+				w = append(w, &queryConditionEq{
+					field: queryField{model: source, field: source.t.Field(i).Name},
+					value: f.Interface(),
+				})
+			}
+		}
+	}
+	if len(w) > 0 {
+		q.condition = append(q.condition, &w)
+	}
+
+	for i, j := 0, v.NumField(); i < j; i++ {
+		f := v.Field(i)
+
+		if !reflect.DeepEqual(f.Interface(), reflect.Zero(f.Type()).Interface()) {
+			if f.Kind() == reflect.Ptr {
+				f = reflect.Indirect(f)
+			}
+
+			if f.Kind() == reflect.Slice {
+				for k := 0; k < f.Len(); k++ {
+					f := f.Index(k)
+
+					if f.Kind() == reflect.Ptr {
+						f = reflect.Indirect(f)
+					}
+
+					join := q.makeJoinForRelation(source, source.t.Field(i).Name)
+
+					q.joins = append(q.joins, *join)
+
+					if err := q.addModelToQuery(join.remote.model, f.Interface()); err != nil {
+						return err
+					}
+				}
+			}
+
+			if f.Kind() == reflect.Struct {
+				join := q.makeJoinForRelation(source, source.t.Field(i).Name)
+
+				q.joins = append(q.joins, *join)
+
+				if err := q.addModelToQuery(join.remote.model, f.Interface()); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (q *selectQuery) makeJoinForRelation(source *Repository, fieldName string) *queryJoin {
+	f, ok := source.t.FieldByName(fieldName)
+	if !ok {
+		return nil
+	}
+
+	t := f.Type
+
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	if t.Kind() == reflect.Slice || t.Kind() == reflect.Array {
+		t = t.Elem()
+
+		if t.Kind() == reflect.Ptr {
+			t = t.Elem()
+		}
+	}
+
+	if t.Kind() != reflect.Struct {
+		return nil
+	}
+
+	v := reflect.Indirect(reflect.New(t))
+
+	other := q.model.d.MustRepository(v.Interface())
+
+	remoteJoinField := ""
+	sourceJoinField := ""
+
+	for i := 0; i < t.NumField(); i++ {
+		rf := t.Field(i).Name
+		sf := fieldName + rf
+
+		if _, ok := source.t.FieldByName(sf); ok {
+			remoteJoinField = rf
+			sourceJoinField = sf
+			break
+		}
+	}
+
+	if remoteJoinField == "" && sourceJoinField == "" {
+		if _, ok := t.FieldByName(source.t.Name() + "Id"); ok {
+			remoteJoinField = source.t.Name() + "Id"
+			sourceJoinField = "Id"
+		}
+	}
+
+	return &queryJoin{
+		source: queryField{
+			model: source,
+			field: sourceJoinField,
+		},
+		remote: queryField{
+			model: other,
+			field: remoteJoinField,
+		},
+	}
 }
--- repository.go
+++ repository.go
@@ -3,7 +3,6 @@ package databam
 import (
 	"reflect"
 
-	"github.com/lann/squirrel"
 	"github.com/serenize/snaker"
 )
 
@@ -112,17 +111,7 @@ func (r Repository) Fetch(out interface{}, where interface{}) error {
 			return ErrIncompatibleType
 		}
 
-		joins, wheres, err := r.addToQuery(&r, where)
-		if err != nil {
-			return err
-		}
-
-		for _, j := range joins {
-			q = q.Join(j)
-		}
-		for _, w := range wheres {
-			q = q.Where(w)
-		}
+		q.addModelToQuery(&r, where)
 	}
 
 	query, parameters, err := q.Compile()
@@ -142,140 +131,4 @@ func (r Repository) Fetch(out interface{}, where interface{}) error {
 	defer rows.Close()
 
 	return r.d.mapper.RowsTo(rows, out)
-}
-
-func (r *Repository) addToQuery(source *Repository, where interface{}) ([]queryJoin, []squirrel.Eq, error) {
-	v := reflect.ValueOf(where)
-
-	joins := []queryJoin{}
-	wheres := []squirrel.Eq{}
-
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
-
-	w := squirrel.Eq{}
-	for i, j := 0, v.NumField(); i < j; i++ {
-		f := v.Field(i)
-
-		if !reflect.DeepEqual(f.Interface(), reflect.Zero(f.Type()).Interface()) {
-			if f.Kind() == reflect.Ptr {
-				f = reflect.Indirect(f)
-			}
-
-			if f.Kind() == reflect.Int || f.Kind() == reflect.String {
-				// w[r.fieldToColumn(source, source.t.Field(i).Name)] = f.Interface()
-			}
-		}
-	}
-	if len(w) > 0 {
-		wheres = append(wheres, w)
-	}
-
-	for i, j := 0, v.NumField(); i < j; i++ {
-		f := v.Field(i)
-
-		if !reflect.DeepEqual(f.Interface(), reflect.Zero(f.Type()).Interface()) {
-			if f.Kind() == reflect.Ptr {
-				f = reflect.Indirect(f)
-			}
-
-			if f.Kind() == reflect.Slice {
-				for k := 0; k < f.Len(); k++ {
-					f := f.Index(k)
-
-					if f.Kind() == reflect.Ptr {
-						f = reflect.Indirect(f)
-					}
-
-					join := r.join(source, source.t.Field(i).Name)
-
-					joins = append(joins, *join)
-
-					if j, w, err := r.addToQuery(join.remote.model, f.Interface()); err != nil {
-						return nil, nil, err
-					} else {
-						joins = append(joins, j...)
-						wheres = append(wheres, w...)
-					}
-				}
-			}
-
-			if f.Kind() == reflect.Struct {
-				join := r.join(source, source.t.Field(i).Name)
-
-				joins = append(joins, *join)
-
-				if j, w, err := r.addToQuery(join.remote.model, f.Interface()); err != nil {
-					return nil, nil, err
-				} else {
-					joins = append(joins, j...)
-					wheres = append(wheres, w...)
-				}
-			}
-		}
-	}
-
-	return joins, wheres, nil
-}
-
-func (r *Repository) join(source *Repository, name string) *queryJoin {
-	f, ok := source.t.FieldByName(name)
-	if !ok {
-		return nil
-	}
-
-	t := f.Type
-
-	if t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-
-	if t.Kind() == reflect.Slice || t.Kind() == reflect.Array {
-		t = t.Elem()
-
-		if t.Kind() == reflect.Ptr {
-			t = t.Elem()
-		}
-	}
-
-	if t.Kind() != reflect.Struct {
-		return nil
-	}
-
-	v := reflect.Indirect(reflect.New(t))
-
-	other := r.d.MustRepository(v.Interface())
-
-	remoteField := ""
-	sourceField := ""
-
-	for i := 0; i < t.NumField(); i++ {
-		rf := t.Field(i).Name
-		sf := name + rf
-
-		if _, ok := source.t.FieldByName(sf); ok {
-			remoteField = rf
-			sourceField = sf
-			break
-		}
-	}
-
-	if remoteField == "" && sourceField == "" {
-		if _, ok := t.FieldByName(source.t.Name() + "Id"); ok {
-			remoteField = source.t.Name() + "Id"
-			sourceField = "Id"
-		}
-	}
-
-	return &queryJoin{
-		source: queryField{
-			model: source,
-			field: sourceField,
-		},
-		remote: queryField{
-			model: other,
-			field: remoteField,
-		},
-	}
 }
